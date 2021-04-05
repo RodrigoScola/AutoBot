3dc32bd3290ba34f758d06599c3f97c5c9197943
rodrigo scola <80076402+RodrigoScola@users.noreply.github.com>
2021-04-04 22:05:05 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// let characters =  ['hello','there','fella'];
+
+// var randomIdeas = (subjarr) => subjarr[Math.floor(Math.random() * subjarr.length)]; 
+
+// let idea = {
+//     characters: randomIdeas(characters)
+// }
